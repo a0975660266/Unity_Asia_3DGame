2c642fe674da23f534b1ac659098ac22d60b863f
a0975660266 <71679957+a0975660266@users.noreply.github.com>
2020-12-15 15:59:56 +0800

    對話框

---
+++ Unity_Asia_3DGame/Assets/scripts/NPC.cs
@@ -0,0 +1,43 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+public class NPC : MonoBehaviour
+{
+    [Header("NPC資料")]
+    public NPCData data;
+    [Header("對話框")]
+    public GameObject dialog;
+    [Header("對話內容")]
+    public Text textContent;
+
+    public bool playerInArea;
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if(other.name == "主機")
+        {
+            playerInArea = true;
+            Dialog();
+        }
+    }
+    private void OnTriggerExit(Collider other)
+    {
+        if (other.name == "主機")
+        {
+            playerInArea = false;
+        }
+    }
+    private void Dialog()
+    {
+        //print(data.dialougA);
+        //for(int i = 0; i < 10;i++)
+        //{
+        //    print("我是迴圈:" + i);
+        //}
+
+        for(int i = 0; i < data.dialougA.Length; i++)
+        {
+            print(data.dialougA[i]);
+        }
+    }
+}
